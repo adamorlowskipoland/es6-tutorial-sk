3933d1dd78d13072f26284f403b2e50337a8315d
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-06 22:47:48 +0200

    more destructuring

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let a = 1;
-let b = 2;
-[a, b] = [b, a];
+let person = {
+    name: 'Thomas',
+    age: 30,
+    city: 'Kraków'
+};
 
-console.log(a, b);
+// let {name, age, city} = person;
+// console.log(name, age, city);
+
+let {name: n, age: a, city: c = 'Warszawa'} = person;
+console.log(n, a, c);
+
