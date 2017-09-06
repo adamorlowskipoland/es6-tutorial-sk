b657b44bacbdff1580c852d7d0ddbd1bd72a14c0
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-06 23:13:09 +0200

    operator spread

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function getValues(...Args) {   // operator REST always at the end
-    return Args.sort();
+// function sumValues(a, b, c) {
+//     return a + b + c;
+// }
+//
+// let values = [3, 2, 7];
+//
+// console.log(sumValues(...values));
 
-}
-
-console.log(getValues('zenek', 'zenek4', 'zenek2', 'zenek0'));
+let array1 = [10, 20, 30];
+let array2 = [40, 50, 60];
+// array1.push(array2);     // this will push whole array into array as one element
+array1.push(...array2);     // this puts every elem from array 2 as indyvidual elem intu array1
+console.log(array1);
