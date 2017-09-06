f254bbaf111e6b655419c255ed97f1d90145f6f5
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-06 23:52:06 +0200

    arrow functions and this explanation

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
-let array = [10, 20, 30, 40, 50];
+// this won't work, wrong 'this' usage
+function Counter() {
+    this.count = 0;
+    setTimeout(function() {
+        this.count++;
+        console.log(this.count);
+    }, 1)
+}
+let a = new Counter();
 
-let sum = array.reduce(function (a, b) {
-    return a + b;
-});
 
-let sum2 = array.reduce((a, b) => a + b);
+// this way will help to fire function correctly but it's a bad practice, you can do better then this
+function Counter1() {
+    let that = this;
+    this.count = 0;
+    setTimeout(function() {
+        that.count++;
+        console.log(that.count);
+    }, 1)
+}
+let b = new Counter1();
 
-console.log(sum);
-console.log(sum2);
+
+// this will work, you bind 'this' in the wanted context
+function Counter2() {
+    this.count = 0;
+    setTimeout(function() {
+        this.count++;
+        console.log(this.count);
+    }.bind(this), 1)
+}
+let c = new Counter2();
+
+
+
+// correct usage of arrow function which uses the same 'this' context
+function Counter3() {
+    this.count = 0;
+    setTimeout(() => {
+        this.count++;
+        console.log(this.count);
+    }, 1)
+}
+
+let d = new Counter3();
+
