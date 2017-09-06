53aa77f36f58b5208a8593246e1dd791447e383e
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-06 21:26:08 +0200

    start working on a string template

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function showVariable() {
-    let x = 2;
-    console.log(x);
-}
-
-showVariable();
+let price = 199;
+let currency = 'PLN';
+let discount = 0.1;
+let label = `Cena wynosi: ${price - price * discount}${currency}`;
+console.log(label);
