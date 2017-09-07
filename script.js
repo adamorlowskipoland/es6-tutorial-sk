3b1a4316ae34b89ed385b8f4e91a951f3cada35b
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-07 23:09:58 +0200

    for of loop used on nodeList

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-let set = new Set();
-
-// values must be unique
-set.add(10);
-set.add(20);
-set.add('OK');
-set.add(true);
-set.add({});
-set.add({});
-
-console.log(set);
-set.delete(10);
-console.log(set.has(10));
-
-for (let v of set) {
-    console.log(v);
-}
+window.onload = function () {
+    const paragraphs = document.querySelectorAll('p');
+    for (const p of paragraphs) {
+        p.className = 'blue';
+    }
+};
