8abc82773fa59b1e83e04b9f806d938409e7958a
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-10 22:20:02 +0200

    creating WeakMap

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function *cities() {
-    yield 'Warsaw';
-    yield 'Wroclaw';
-    yield 'Poznan';
-}
+let m = new WeakMap();
 
-let places = cities();
+let object1 = {id: 1};
+let object2 = {id: 2};
+let object3 = {id: 3};
+// m.set(1, 'React');   // this is how you set Map
+m.set(object1, 'React');    // in WeakMap you need to set an Object
+m.set(object2, 'Ember');
+m.set(object3, 'Angular');
+console.log(m);
 
-for (let city of places) {
-    console.log(city);
+// object3 = null;  // this deletes all references
+
+console.log(m.get(object3));
+
+for (const v of m) {
+    console.log(v);
 }
