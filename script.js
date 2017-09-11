849b6c9d9219f30b563f49c4551f3cb338d58bf2
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-12 00:41:59 +0200

    creating class in two ways

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-let s = new WeakSet();
+// class Article {
+//
+// }
+//
+// const art = new Article();
 
-// s.add(1);  // in WeakSet you can use only Objects
+const Article = class ArticleClass {
 
-let obj1 = {name: 'Header'};
-let obj2 = {name: 'Main'};
-let obj3 = {name: 'Footer'};
-s.add(obj1);
-s.add(obj2);
-s.add(obj3);
+};
 
-console.log(s);
-console.log(s.delete(obj1));
-console.log(s.has(obj1));
+const art = new Article;
 
-// you can't check WeakSet's length, so you can not iterate through it
+console.log(art);
