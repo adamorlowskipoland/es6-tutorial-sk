4beb6bdb9469808946fb0ee30233bd7e1c39815b
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-12 00:57:50 +0200

    add method to class and static method to class

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ class Article {
         this.id = id;
         this.title = title;
     }
-//    only one constructor in the class
+    getId() {
+        return this.id;
+    }
+//    static method
+    static compareId(a, b) {
+        const id1 = a.id;
+        const id2 = b.id;
+        if (id1 > id2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 const art = new Article(95, 'Filtry w CSS3');
 
 console.log(art.id, art.title);
+console.log(art.getId());
 
+const art1 = new Article(59, 'Wprowadzenie do HTML5');
+
+console.log(Article.compareId(art, art1));
