aa79ff9a87953f86217cfe3b779eb259d0ad53fd
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-12 00:48:38 +0200

    add coment to last commit

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ class Article {
         this.id = id;
         this.title = title;
     }
+//    only one constructor in the class
 }
 
 const art = new Article(95, 'Filtry w CSS3');
 
 console.log(art.id, art.title);
+
