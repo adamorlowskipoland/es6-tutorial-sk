064e6f6d8451bba75c3f67665a0b6f8a8a9ffc24
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-13 01:45:39 +0200

    create promise

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
-//  es5 way
-function getPerson(name, age) {
-    return {
-        name: name,
-        age: age,
-        checkAge: function () {
-            if (this.age >= 18) {
-                return true
-            } else {
-                return false;
-            }
-        }
-    };
-}
+let x = 1;
 
-//  es6 way
-function getPerson1(name, age) {
-    return {
-        name,
-        age,
-        checkAge() {
-            return this.age >= 18;
-        },
-        //  creating dynamic content
-        [`user${name}${age}`]: 'OK'
-    };
-}
+let p = new Promise(function (resolve, reject) {
+    if (x === 1) {
+        resolve('OK!');
+    } else {
+        reject(Error('Błąd'));
+    }
+});
 
-let p = getPerson('Tomek', 30);
 console.log(p);
-console.log(p.checkAge());
 
-let p1 = getPerson1('Thomas', 31);
-console.log(p1);
-console.log(p1.checkAge());
-console.log(p1.userThomas31);
+p.then(function (result) {
+    console.log(result);    // OK!
+}).catch(function (error) {
+    console.log(error)
 
-
+});
