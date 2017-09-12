090cda4d5198df0c3fbbf5c31eb895b4485dbf22
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-13 01:52:58 +0200

    Promise.all example

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let x = 1;
-
-let p = new Promise(function (resolve, reject) {
-    if (x === 1) {
-        resolve('OK!');
-    } else {
-        reject(Error('Błąd'));
-    }
+let p1 = new Promise((resolve, reject) => {
+    resolve('OK1');
+});
+let p2 = new Promise((resolve, reject) => {
+    resolve('OK2');
 });
 
-console.log(p);
-
-p.then(function (result) {
-    console.log(result);    // OK!
-}).catch(function (error) {
-    console.log(error)
-
+let p3 = new Promise((resolve, reject) => {
+    // resolve('OK3');
+    reject('Błąd3');
 });
+
+// all Promises needs to be resolved for this to work
+
+Promise.all([p1, p2, p3])
+    .then(results => {
+        console.log(results);
+    })
+    .catch(err => {
+       console.log(err);
+    });
+
