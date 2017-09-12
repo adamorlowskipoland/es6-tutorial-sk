cbecf4b965d29a4b7840ceb1b92e2279fcac7955
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-12 19:35:59 +0200

    using symbols

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-class Article {
-    constructor(id, title) {
-        this.id = id;
-        this.title = title;
-    }
-    getId() {
-        return this.id;
-    }
-//    static method
-    static compareId(a, b) {
-        const id1 = a.id;
-        const id2 = b.id;
-        if (id1 > id2) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+//  without a 'new'
+let s1 = Symbol('Logowanie');
+let s2 = Symbol('Logowanie');
+
+//  Symbols are always unique
+console.log(s1 === s2);
+// kinda like objects
+console.log({} === {});
+
+let person = {
+    name: 'Pawel',
+    age: 40,
+    city: 'Warsaw'
+};
+
+let s = Symbol();
+person[s] = '1376';
+
+for (let i in person) {
+    // this will show in console only properties, without symbol
+    console.log(i, person[i]);
 }
 
-const art = new Article(95, 'Filtry w CSS3');
+console.log(person);
 
-console.log(art.id, art.title);
-console.log(art.getId());
-
-const art1 = new Article(59, 'Wprowadzenie do HTML5');
-
-//  static method use on the class
-console.log(Article.compareId(art, art1));
-
-class BlogArticle extends Article {
-    constructor(id, tittle, category) {
-        super(id, tittle);
-        this.category = category;
-    }
-}
-
-const blog1 = new BlogArticle(23, 'Tworzenie aplikacji z Angular', 'Frameworks');
-
-console.log(blog1);
-console.log(blog1.getId());
+// to console log symbol from object you need to do as follow
+console.log(person[s]);
+console.log(Object.getOwnPropertySymbols(person));
