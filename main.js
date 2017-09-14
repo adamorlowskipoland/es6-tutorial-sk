ba6ad71b18618df088ef66ae1115d22305accc57
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-14 23:54:37 +0200

    2 ways of importing and exporting modules

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-import {env} from './module.js'
-
+import {env, sumNumbers} from './module.js'
 console.log(env);
+console.log(sumNumbers(3, 9));
+
+// 2nd way of importing
+import * as module from './module.js'
+console.log(module.env);
+console.log(module.sumNumbers(3, 9));
--- module.js
+++ module.js
@@ -1,1 +1,11 @@
 export const env = 'PRODUCTION';
+export function sumNumbers(a, b) {
+    return a + b;
+}
+
+//  2nd way of exporting
+const env2 = 'PRODUCTION';
+function sumNumbers2(a, b) {
+    return a + b;
+}
+export {env2, sumNumbers2};
