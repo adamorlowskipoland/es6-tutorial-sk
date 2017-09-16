3ed39bafe117e9e28b1841a7eec56cc9fff6f05c
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-16 21:21:07 +0200

    Typed Array examples

--- src/module.js
+++ src/module.js
@@ -1,9 +1,14 @@
-const myString = '\u0041';
-const myString1 = '\u0040\u260e\u2764';
-console.log(myString);
-console.log(myString1);
-console.log(myString1.length);
+//  RULES
+//  Uint8Array - Unside intiger 8 length type Array
+//  ignors after dot; bigger then 8 translate to smaller by own algorythm, strings are translated to numbers
+const typedArr = new Uint8Array([-1, 0, 1, 2, 3, 4.333, 5.343, 9, '0', 30000]);
+console.log(typedArr);
+console.log(typedArr.length);
 
-for (const c of myString1) {
-    console.log(c);
+for (const b of typedArr) {
+    console.log(b);
 }
+
+const typedArr1= new Float32Array([-1, 0, 1, 2, 3, 4.333, 5.343, 9, '0', 30000]);
+console.log(typedArr1);
+
