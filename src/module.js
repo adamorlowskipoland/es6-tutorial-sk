895c7a969aab8e3eeb279b1475a216cc8e2b00ec
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-17 11:51:32 +0200

    regexp exec + sticky example

--- src/module.js
+++ src/module.js
@@ -1,14 +1,16 @@
-//  RULES
-//  Uint8Array - Unside intiger 8 length type Array
-//  ignors after dot; bigger then 8 translate to smaller by own algorythm, strings are translated to numbers
-const typedArr = new Uint8Array([-1, 0, 1, 2, 3, 4.333, 5.343, 9, '0', 30000]);
-console.log(typedArr);
-console.log(typedArr.length);
+const string = 'ok ok ok OK';
+const pattern = /ok/g;
+console.log(pattern.exec(string));
+console.log(pattern.lastIndex);
+console.log(pattern.exec(string));
+console.log(pattern.lastIndex);
+console.log(pattern.exec(string));
+console.log(pattern.lastIndex);
 
-for (const b of typedArr) {
-    console.log(b);
-}
-
-const typedArr1= new Float32Array([-1, 0, 1, 2, 3, 4.333, 5.343, 9, '0', 30000]);
-console.log(typedArr1);
-
+const patterny = /ok/y; // sticky
+console.log(patterny.exec(string));
+console.log(patterny.lastIndex);
+console.log(patterny.exec(string));
+console.log(patterny.lastIndex);
+console.log(patterny.exec(string));
+console.log(patterny.lastIndex);
